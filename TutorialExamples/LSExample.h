5292f324eac16a72469539cd95e7892c33d7ce77
Seongkwan Yoon <lingostar.joker@gmail.com>
2013-03-30 20:14:46 +0900

    LSExample File Added

---
+++ TutorialExamples/LSExample.h
@@ -0,0 +1,23 @@
+//
+//  LSExample.h
+//  TutorialExamples
+//
+//  Created by Lingostar on 13. 3. 30..
+//  Copyright (c) 2013년 Lingostar. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LSExample : NSObject
+
+@property int idOfExample;
+@property NSString *titleOfExample;
+@property NSString *descriptionOfExample;
+@property NSString *purposeOfExample;
+@property NSString *chapterOfExample;
+@property NSString *classOfExample;
+@property NSString *thumbnailPath;
+@property NSString *iconPath;
+@property NSArray *screenShotPathArray;
+
+@end
---
+++ TutorialExamples/LSExample.m
@@ -0,0 +1,13 @@
+//
+//  LSExample.m
+//  TutorialExamples
+//
+//  Created by Lingostar on 13. 3. 30..
+//  Copyright (c) 2013년 Lingostar. All rights reserved.
+//
+
+#import "LSExample.h"
+
+@implementation LSExample
+
+@end
